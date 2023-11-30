cec330d6a1f9c10d4d4a87876bf0015ec225db1d
miura-code <smiuymiu0official0@gmail.com>
2023-11-30 22:27:53 +0900

    [fix] データの参照場所を変更

--- cnn/test.py
+++ cnn/test.py
@@ -17,7 +17,7 @@ from model import NetworkCIFAR as Network
 
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='/home/miura/lab/data', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
